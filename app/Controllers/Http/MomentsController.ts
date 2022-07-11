67f0214501c0b5b068c052e3c04a83f8e2b38c34
Gustavo Medeiros <gugamedeiros517@gmail.com>
2022-07-11 14:12:52 -0300

    adiciona o metodo create() no controller de Moment

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -1,4 +1,16 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Moment from 'App/Models/Moment'
 
 export default class MomentsController {
+  public async store({ request, response }: HttpContextContract) {
+    const momentData = request.body()
+    const results = await Moment.create(momentData)
+
+    response.status(201)
+
+    return {
+      message: 'Moment created.',
+      insertedMoment: results,
+    }
+  }
 }
