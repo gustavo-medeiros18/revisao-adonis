0cb9951b41beb2df0084ae260483539af6ac58b5
Gustavo Medeiros <gugamedeiros517@gmail.com>
2022-07-11 14:55:36 -0300

    adiciona rotina de inserção de imagem no método create() do controller de Moment

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -1,11 +1,28 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Moment from 'App/Models/Moment'
+import Application from '@ioc:Adonis/Core/Application'
+import { v4 } from 'uuid'
 
 export default class MomentsController {
+  private validatonRules = {
+    types: 'image',
+    size: '5mb',
+  }
+
   public async store({ request, response }: HttpContextContract) {
     const momentData = request.body()
-    const results = await Moment.create(momentData)
+    const image = request.file('image', this.validatonRules)
 
+    if (image) {
+      const imageName = `${v4()}.${image.extname}`
+      await image.move(Application.tmpPath('uploads'), {
+        name: imageName,
+      })
+
+      momentData.image = imageName
+    }
+
+    const results = await Moment.create(momentData)
     response.status(201)
 
     return {
