7d50c0edc07ae45f88fc71aa032f9990e40bcb60
Gustavo Medeiros <gugamedeiros517@gmail.com>
2022-07-14 17:44:46 -0300

    remove o teste padrão feito pelo adonis

--- tests/functional/hello_world.spec.ts
+++ tests/functional/hello_world.spec.ts
@@ -1,11 +1,5 @@
 import { test } from '@japa/runner'
 
-test('display welcome page', async ({ client }) => {
-  const response = await client.get('/api')
-
-  response.assertBodyContains({ hello: 'world' })
-})
-
 test('comment creation', async ({ client }) => {
   const response = await client.post('/api/moments/1/comments').json({
     username: 'gustavo.medeiros',
